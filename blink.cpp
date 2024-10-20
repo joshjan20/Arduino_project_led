a51feef66ff430886b126a32abccb9f4228f5c37
JJranji <joshjan20@gmail.com>
2024-10-20 22:34:38 +0200

    update

---
+++ blink.cpp
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Initialize the built-in LED pin as an output
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(5000); // Wait for one second
+  
+  // Turn the LED off
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(5000); // Wait for one second
+}
